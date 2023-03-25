d44c6ce1a53cf18c6dba60908acc255ec648e1de
alexchunlin <alexstephens38@gmail.com>
2023-03-24 23:58:51 -0400

    completed exercise A

--- excercises/a_variables/variables/src/main.rs
+++ excercises/a_variables/variables/src/main.rs
@@ -1,3 +1,14 @@
+// make a constant called STARTING_MISSILES and READY_AMOUNT
+// make them type i32 and initialize missiles and ready variables from them
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
 fn main() {
-    println!("Hello, world!");
+    
+    // initialize missiles and ready variables in one line and annotate them as i32
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+
+    // print out the number of missiles and ready missiles
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    println!("{} missiles left", missiles - ready);
 }
