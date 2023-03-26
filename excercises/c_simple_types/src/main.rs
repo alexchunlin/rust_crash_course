3f9531631dc35171ec6054b352df130198f6cf60
alexchunlin <alexstephens38@gmail.com>
2023-03-25 23:50:31 -0400

    removed completed instructions

--- excercises/c_simple_types/src/main.rs
+++ excercises/c_simple_types/src/main.rs
@@ -18,20 +18,6 @@ fn main() {
     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
     on_off(mess.2[1].0);
 
-    // 5.  What a mess -- functions in a binary! Let's get organized!
-    //
-    // - Make a library file (src/lib.rs)
-    // - Move all the functions (except main) into the library
-    // - Make all the functions public with `pub`
-    // - Bring all the functions into scope using use statements. Remember, the name of the library
-    //   is defined in Cargo.toml.  You'll need to know that to `use` it.
-    //
-    // `cargo run` should produce the same output, only now the code is more organized. 🎉
-
-    // Challenge: Uncomment the line below, run the code, and examine the
-    // output. Then go refactor the print_distance() function according to the
-    // instructions in the comments inside that function.
-
     print_distance(coords);
 }
 
